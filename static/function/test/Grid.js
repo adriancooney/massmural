55f0f5dd5620b72397eddb9ba34ad33d8917471b
Adrian Cooney <cooney.adrian@gmail.com>
2014-03-25 03:10:41 +0000

    Small tests and some tiles

---
+++ static/function/test/Grid.js
@@ -0,0 +1,11 @@
+describe("Grid", function() {
+	var g = new Grid(300, 400, 100, 100);
+	describe("Constructor", function() {
+		// g.pan(-300, -300);
+		g.render();
+	});
+
+	describe("#pan", function() {
+
+	});
+});
